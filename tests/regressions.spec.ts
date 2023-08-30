71b253a1e7b317291cdedfb2ce2c685608aaad58
alexgagnon <alex@monad.media>
2023-08-30 13:43:58 -0500

    wait for DCL for CLS

--- tests/regressions.spec.ts
+++ tests/regressions.spec.ts
@@ -5,6 +5,7 @@ import { test, expect } from '@playwright/test';
   test(`Visual Regressions for ${path}`, async ({ page }) => {
     await page.goto(`http://localhost:4322${path}`);
     await page.locator('.fonts-loaded'); // wait for fonts to load
+    await page.locator('.fancy-button');
     expect(await page.screenshot()).toMatchSnapshot({threshold: 0.1});
   });
 })
