7896f3563c3e45fd5a1b29e72365aa124fea399c
alexgagnon <alex@monad.media>
2023-08-30 10:28:23 -0500

    fix github actions

--- tests/regressions.spec.ts
+++ tests/regressions.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+// TODO: /about is flakey because of lazy loaded images
 ['', '/about', '/resume'].map(path => {
   test(`Visual Regressions for ${path}`, async ({ page }) => {
     await page.goto(`http://localhost:4322${path}`);
