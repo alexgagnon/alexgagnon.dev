38464bb49ec983c32c6b97655f7c29ef3b53ec46
alexgagnon <alex@monad.media>
2023-08-31 12:32:46 -0500

    fix slowdowns from GA and hotjar

--- tests/regressions.spec.ts
+++ tests/regressions.spec.ts
@@ -1,9 +1,15 @@
 import { test, expect } from '@playwright/test';
 
+const pages = {
+  "Home": "/",
+  "About": "/about",
+  "Resume": "/resume",
+};
+
 // TODO: /about is flakey because of lazy loaded images
-['', /*'/about', */'/resume'].map(path => {
-  test(`Visual Regressions for ${path}`, async ({ page }) => {
-    await page.goto(`http://localhost:4322${path}`);
+Object.entries(pages).map(([label, path]) => {
+  test(`Visual Regressions for ${label} (${path})`, async ({ page }) => {
+    await page.goto(`http://localhost:4321${path}`);
     await page.locator('.fonts-loaded'); // wait for fonts to load
     await page.locator('.fancy-button');
     expect(await page.screenshot()).toMatchSnapshot({threshold: 0.1});
